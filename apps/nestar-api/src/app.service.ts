86ccfa288593c6a1f039a2f99a1eaded6cd7243b
“Shonedxb” <firdavs.cra@gmail.com>
2025-06-06 00:20:28 +0400

    feat: Intergration of GraphQLModule and build first GraphQL api

--- apps/nestar-api/src/app.service.ts
+++ apps/nestar-api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to Nestart Api Server';
+    return 'Welcome to Nestart Rest Api Server';
   }
 }
