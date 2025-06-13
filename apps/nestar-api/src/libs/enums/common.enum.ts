1c022c6e8e04bf73f84c00c61c0f2ddc5b2b7b54
“Shonedxb” <firdavs.cra@gmail.com>
2025-06-13 23:10:56 +0400

    feat: Develop Global Error Handling System

--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -7,9 +7,12 @@ export enum Message {
     UPLOAD_FAILED = "Upload failed!",
     BAD_REQUEST = "Bad request!",
 
+    USED_MEMBER_NICK_OR_PHONE = "Member nick or phone is already used!",
+    USED_MEMBER_EMAIL = "Member email is already used!",
     NO_MEMBER_NICK = "No member with that membernick!",
     BLOCKED_USER = "You have been blocked!",
     WRONG_PASSWORD = "Wrong password, try again!",
+    MEMBER_NOT_EXIST = "Member does not exist!",
     NOT_AUTHENTICATED = "You are not authenticated, please login first!",
     TOKEN_NOT_EXIST = "Token not exist!",
     ONLY_SPECIFIC_ROLES_ALLOWED = "Allowed only for members with specific roles!",
