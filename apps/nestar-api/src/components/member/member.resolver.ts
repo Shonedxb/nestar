2388fd2460e686ba2af755d084398dd0498e2e60
“Shonedxb” <firdavs.cra@gmail.com>
2025-06-14 20:11:58 +0400

    feat: Develop logging via global interceptors

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,25 +10,14 @@ export class MemberResolver {
 
     @Mutation(() => Member)
     public async signup(@Args('input') input: MemberInput): Promise<Member> {
-        try {
-            console.log('Mutation: signup');
-            console.log('input:', input);
-            return this.memberService.signup(input);
-        } catch (err) {
-            console.log('Error in signup:', err);
-            throw new InternalServerErrorException(err);
-        }
+        console.log('Mutation: signup');
+        return this.memberService.signup(input);
     }
 
     @Mutation(() => Member)
-        public async login(@Args('input') input: LoginInput): Promise<Member> {
-            try {
-            console.log('Mutation: login');
-            return this.memberService.login(input);
-        } catch (err) {
-            console.log('Error in login:', err);
-            throw new InternalServerErrorException(err);
-        }
+    public async login(@Args('input') input: LoginInput): Promise<Member> {
+        console.log('Mutation: login');
+        return this.memberService.login(input);
     }
 
     @Mutation(() => String)
