428b93b130305914a1770dfac3a5dc0ffc4c1d6f
“Shonedxb” <firdavs.cra@gmail.com>
2025-06-24 20:22:16 +0400

    feat: Develop GetProperty api logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,13 +1,15 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
 import { Property } from '../../libs/dto/property/property';
-import { Roles } from '../auth/decorators/roles.decorator';
+import { PropertyInput } from '../../libs/dto/property/property.input';
 import { MemberType } from '../../libs/enums/member.enum';
+import { Roles } from '../auth/decorators/roles.decorator';
 import { UseGuards } from '@nestjs/common';
 import { RolesGuard } from '../auth/guards/roles.guard';
-import { PropertyInput } from '../../libs/dto/property/property.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class PropertyResolver {
@@ -21,5 +23,15 @@ export class PropertyResolver {
         input.memberId = memberId;
         return await this.propertyService.createProperty(input);  
     }
-    
+
+    @UseGuards(WithoutGuard)
+    @Query((returns) => Property)
+    public async getProperty(
+        @Args('propertyId') input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Property> {
+        console.log('Query: getProperty');
+        const propertyId = shapeIntoMongoObjectId(input);
+        return await this.propertyService.getProperty(memberId, propertyId);    
+    }
 }
--- apps/nestar-api/src/libs/dto/property/property.ts
+++ apps/nestar-api/src/libs/dto/property/property.ts
@@ -2,6 +2,7 @@ import { Field, Int, ObjectType } from "@nestjs/graphql";
 import { ObjectId } from "mongoose";
 import { PropertyLocation, PropertyStatus, PropertyType } from "../../enums/property.enum";
 import { Prop } from "@nestjs/mongoose";
+import { Member } from "../member/member";
 
 @ObjectType()
 export class Property {
@@ -76,4 +77,9 @@ export class Property {
 
     @Field(() => Date)
     updatedAt: Date;
+
+    /** from Aggregattion */
+
+    @Field(() => Member, { nullable: true })
+    memberData?: Member
 }
